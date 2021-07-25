cd7a705958977946b3f1a96f8818c73c904ebaa0
egzon.korenica <egzon.korenica@bevuta.com>
2021-07-25 15:34:50 +0200

    v0.1

---
+++ TTS.py
@@ -0,0 +1,32 @@
+# Install dependencies
+# authenticate
+# convert a string
+# convert fromm a file
+# using new language models
+
+url = 'https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/58f23641-330d-4584-a281-1fa87ff5431a'
+apikey = 'EWtDUdU2pOw1TCVwiQtSPX7uSva3QSg8QBXzJpFX9Tbx'
+
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+# setup service
+authenticator = IAMAuthenticator(apikey)
+#new tts service
+tts = TextToSpeechV1(authenticator=authenticator)
+# set service url
+tts.set_service_url(url)
+
+with open('./speech.wav', 'wb') as audio_file:
+    res = tts.synthesize("Hello WORLD WORLD WORLD WORLD", accept='audio/wav', voice='en-US_LisaV3Voice').get_result()
+    audio_file.write(res.content)
+
+with open("covid.txt", 'r') as f:
+    text = f.readlines()
+
+text = [line.replace('\n', '') for line in text]
+text = ''.join(str(line) for line in text)
+
+with open('./covid.mp3', 'wb') as audio_file:
+    res = tts.synthesize(text, accept='audio/wav', voice='en-US_LisaV3Voice').get_result()
+    audio_file.write(res.content)
